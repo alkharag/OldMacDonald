2baf74856dd045c340a1f50cea2d61d9f468acbc
alkharag <kharag.alex_sa@yahoo.com>
2014-10-27 11:24:58 -0700

    First try

--- build-tmp/source/OldMacDonald.java
+++ build-tmp/source/OldMacDonald.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OldMacDonald extends PApplet {
+
 //your variable declarations here
 
 	public void setup() 
@@ -70,7 +86,7 @@ interface Animal
   public Chick(String type, String sound)    
   {         
      
-     if (Math.random()>0.5)
+     if (Math.random()>0.5f)
     {
       myType = "Chick";   
        mySound = "Cluck!";  
@@ -85,4 +101,13 @@ interface Animal
 
   public String getSound(){return mySound;}     
   public String getType(){return myType;} 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OldMacDonald" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
